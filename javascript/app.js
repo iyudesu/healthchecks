5970374c232df46f24429582cc296cd941c518b4
chadchayut <chadchayut@jumpbox.co>
2025-04-08 23:28:26 +0700

    build: dockerfile and change app port to 80 to clarify node and container port for Node.js

--- javascript/app.js
+++ javascript/app.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const fs = require('fs');
 const app = express();
-const port = 8080;
+const port = 80;
 
 app.get('/', (req, res) => {
   res.send('Hello, this is code from Node.js!');
